22f43a92ee348e912d13cba45dfee4a55370fd01
fingoldin <vassilioskaxiras@gmail.com>
2017-10-10 15:08:12 +0000

    add round data

--- scripts/dbdata.sql
+++ scripts/dbdata.sql
@@ -16,6 +16,16 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Dumping data for table `round`
+--
+
+LOCK TABLES `round` WRITE;
+/*!40000 ALTER TABLE `round` DISABLE KEYS */;
+INSERT INTO `round` VALUES (1,1,30,1,'Sprint Round',1),(1,2,2,2,'Target, Round 1',1),(1,3,2,2,'Target, Round 2',1),(1,4,2,2,'Target, Round 3',1),(1,5,2,2,'Target, Round 4',1),(1,6,10,2,'Team Round',0),(2,7,3,2,'Individual 1',1),(2,8,3,2,'Individual 2',1),(2,9,3,2,'Individual 3',1),(2,10,3,2,'Individual 4',1),(2,11,3,2,'Individual 5',1),(2,12,9,4,'Team',0);
+/*!40000 ALTER TABLE `round` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping data for table `competition_type`
 --
 
@@ -34,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-09 14:28:01
+-- Dump completed on 2017-10-10 15:00:13
